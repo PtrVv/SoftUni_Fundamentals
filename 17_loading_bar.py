44b4884a6f3359f43c2d7fe38ecc8db7b4d8f633
Peter <147493833+PtrVv@users.noreply.github.com>
2023-10-13 15:05:20 +0300

    Add files via upload

---
+++ 17_loading_bar.py
@@ -0,0 +1,26 @@
+number = int(input())
+
+percentage_list = []
+str_of_number = str(number)
+get_value = str_of_number[0]
+
+if number >= 100:
+    get_value = 10
+
+
+def loading_bar(num: int):
+    for _ in range(int(get_value)):
+        percentage_list.append("%")
+    while len(percentage_list) < 10:
+        percentage_list.append(".")
+    loading_list = ''.join(map(str, percentage_list))
+    return loading_list
+
+
+result = loading_bar(number)
+if number < 100:
+    print(f"{number}% [{result}]")
+    print(f"Still loading...")
+else:
+    print(f"{number}% Complete!")
+    print(f"[{result}]")
