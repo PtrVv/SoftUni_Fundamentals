abc40af6d613bdf251f501ceba6fa232049e35ad
Peter <147493833+PtrVv@users.noreply.github.com>
2023-10-13 15:06:03 +0300

    Add files via upload

---
+++ 08_decipher_this.py
@@ -0,0 +1,27 @@
+secret_message = input().split()
+
+
+def swap_position(word, pos1, pos2):
+    word[pos1], word[pos2] = word[pos2], word[pos1]
+    return word
+
+
+deciphered_list = []
+joined_list = []
+temp_list = []
+for element in secret_message:
+    temp_list.clear()
+    joined_list.clear()
+    for i in range(len(element)):
+        if element[i].isnumeric():
+            temp_list.append(element[i])
+        else:
+            joined_list.append(element[i])
+
+    pos1, pos2 = 0, -1
+    swap_position(joined_list, pos1, pos2)
+    joined = "".join(temp_list)
+    joined_list.insert(0, chr(int(joined)))
+    deciphered_list.append("".join(joined_list))
+
+print(" ".join(deciphered_list))
