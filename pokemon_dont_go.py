57c102b67950a8e3a79f879bf443bf7342b07bda
Peter <147493833+PtrVv@users.noreply.github.com>
2023-10-17 19:30:42 +0300

    Update pokemon_dont_go.py

--- pokemon_dont_go.py
+++ pokemon_dont_go.py
@@ -2,34 +2,27 @@
 # The program ends when the sequence has no elements left.(there are no more pokemon left to be caught)
 distance = [int(digit) for digit in input().split(" ")]
 
-copy_of_list = distance[:]
-
 removed_element = 0
 sum_of_removed = 0
-track_index = []
-while len(copy_of_list) > 0:
+while len(distance) > 0:
     current_index = int(input())
-    track_index.append(current_index)
 
     if current_index < 0:
-        removed_element = copy_of_list[0]
+        removed_element = distance[0]
         sum_of_removed += removed_element
-        copy_of_list[0] = copy_of_list[-1]
-        copy_of_list[-1] = copy_of_list[0]
-        copy_of_list = [x + removed_element if x <= removed_element else x - removed_element for x in copy_of_list]
-    elif current_index > len(copy_of_list) - 1:
-        removed_element = copy_of_list[-1]
+        distance[0] = distance[-1]
+        distance[-1] = distance[0]
+        distance = [x + removed_element if x <= removed_element else x - removed_element for x in distance]
+        
+    elif current_index > len(distance) - 1:
+        removed_element = distance[-1]
         sum_of_removed += removed_element
-        copy_of_list[-1] = copy_of_list[0]
-        copy_of_list = [x + removed_element if x <= removed_element else x - removed_element for x in copy_of_list]
+        distance[-1] = distance[0]
+        distance = [x + removed_element if x <= removed_element else x - removed_element for x in distance]
 
     else:
-        removed_element = copy_of_list.pop(current_index)
+        removed_element = distance.pop(current_index)
         sum_of_removed += removed_element
-        for i in range(len(copy_of_list)):
-            if copy_of_list[i] <= removed_element:
-                copy_of_list[i] += removed_element
-            elif copy_of_list[i] > removed_element:
-                copy_of_list[i] -= removed_element
+        distance = [x + removed_element if x <= removed_element else x - removed_element for x in distance]
 
 print(sum_of_removed)
