2a9674db8ba02e81bea713057478bb10b3bae25b
Peter <147493833+PtrVv@users.noreply.github.com>
2023-11-21 13:51:37 +0200

    Add files via upload

---
+++ 06_extract_the_links.py
@@ -0,0 +1,11 @@
+import re
+
+text_links = input()
+
+pattern = r"(?i)(www.([A-Za-z0-9\-]+)(\.[a-z]+)+)\b"
+while text_links:
+
+    matches = re.findall(pattern, text_links)
+    for link in matches:
+        print("".join(link[0]))
+    text_links = input()
